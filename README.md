6296f4e77bf6246df5d645ad2aa8108a652d08da
Panslothda <19254254+Aureliolo@users.noreply.github.com>
2023-09-04 22:59:38 +0200

    added different amount of days in between

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Discord Countdown Bot
 
-This is a Discord bot that updates multiple channel names to show a countdown to specified times in UTC.
+This is a Discord bot that updates multiple channel names to show a countdown to specified times in UTC, with optional repetition every X days.
 
 ## Usage
 
@@ -11,9 +11,9 @@ This is a Discord bot that updates multiple channel names to show a countdown to
 ## Environment Variables
 
 - `TOKEN`: Your Discord bot's token.
-- `CHANNELS_TIMESTAMPS`: A comma-separated list of channel IDs with target times in UTC. Format: `channel_id:hour:minute`.
+- `CHANNELS_TIMESTAMPS`: A comma-separated list of channel IDs with target times in UTC and repetition days. Format: `channel_id:hour:minute:repeat_days`.
 
-Example: `1234567890:15:30,9876543210:20:45`
+Example: `1234567890:15:30:7,9876543210:20:45:1`
 
 ## Docker Commands
 
--- discord_bot.py
+++ discord_bot.py
@@ -15,14 +15,20 @@ async def update_channel():
 
         for data in channel_data:
             try:
-                channel_id, target_hour, target_minute = map(int, data.split(':'))
+                channel_id, target_hour, target_minute, repeat_days = map(int, data.split(':'))
             except ValueError:
                 print(f"Skipping invalid data: {data}")
                 continue
 
             target_time = now.replace(hour=target_hour, minute=target_minute, second=0, microsecond=0)
-            if now > target_time:
-                target_time += timedelta(days=1)
+            
+            # Calculate the number of days from the target time to now, and add the appropriate number of days to the target time
+            delta_days = (now - target_time).days
+            if delta_days >= 0:
+                cycles_passed = delta_days // repeat_days
+                next_cycle = cycles_passed + 1
+                days_to_add = next_cycle * repeat_days
+                target_time += timedelta(days=days_to_add)
 
             time_left = target_time - now
             hours, remainder = divmod(time_left.seconds, 3600)
